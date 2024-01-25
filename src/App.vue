bdafef67e95d01d951d489a855335ff972ab3244
liuming163 <lm18602904067@163.com>
2024-01-25 14:41:47 +0800

    更换官网logo

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <!-- <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
+    </a> -->
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
