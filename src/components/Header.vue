de16f628360dcad93a3f1b2e005481777087b8e7
liuming163 <lm18602904067@163.com>
2024-01-26 09:56:11 +0800

    修改header样式布局

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,75 @@
 <template>
-  <div class="container">header</div>
+  <div class="container">
+    <div class="left">
+      <img src="../assets/imgs/title.png" />
+    </div>
+    <ul class="right">
+      <li v-for="nav in navList">
+        <router-link :to="nav.path">{{ nav.name }}</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const navList = [
+  {
+    path: "/home",
+    name: "首页",
+  },
+  {
+    path: "/classiccases",
+    name: "经典案例",
+  },
+  {
+    path: "/enterprisedevelopment",
+    name: "企业发展",
+  },
+  {
+    path: "/about",
+    name: "关于我们",
+  },
+  {
+    path: "/cooperation",
+    name: "商务合作",
+  },
+];
+</script>
 
-<style scoped lang="sass"></style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  height: 110px;
+  margin: 0 100px 0 20px;
+  padding: 0 10px;
+  background-color: #ccc;
+
+  .left {
+    width: 300px;
+    line-height: 110px;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .right {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 600px;
+    font-size: 16px;
+    font-weight: 700;
+    // background-color: #30ac91;
+
+    li > a {
+      padding: 15px 0;
+
+      &:hover {
+        border-bottom: 2px solid #3384fb;
+      }
+    }
+  }
+}
+</style>
