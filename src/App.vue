7f4f13e57f2b03bab568d0fdaff7118af3c8eb6c
liuming163 <lm18602904067@163.com>
2024-01-26 16:28:44 +0800

    新增footer区域布局

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header></Header>
   <!-- 路由出口 -->
   <router-view></router-view>
+  <Footer></Footer>
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container-footer">
+    <div class="left">
+      <h2>北京和晞科技有限公司</h2>
+      <p>地址: 北京市大兴区首开万科中心1313</p>
+      <p>电话: 010-88888888</p>
+      <p>邮箱: business@hxdaybreak.com</p>
+    </div>
+    <div class="right">
+      <img src="../assets/imgs/weibo.png" alt="" />
+      <p>微博</p>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style lang="scss" scoped>
+.container-footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 260px;
+  height: 280px;
+  color: #fff;
+  font-size: 16px;
+  background-color: #282828;
+
+  .left {
+    h2 {
+      margin-bottom: 20px;
+    }
+  }
+
+  .right {
+    text-align: center;
+    img {
+      width: 120px;
+      height: 120px;
+    }
+    p {
+      margin-top: 20px;
+    }
+  }
+}
+</style>
