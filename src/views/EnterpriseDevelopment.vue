5df120ff1f97eccaa6f42894dfb75caa15a8e3ea
liuming163 <lm18602904067@163.com>
2024-02-01 13:56:17 +0800

    企业发展页面添加banner图

--- src/views/EnterpriseDevelopment.vue
+++ src/views/EnterpriseDevelopment.vue
@@ -1,7 +1,30 @@
 <template>
-    <div class="enterprise-development">enterprise_development</div>
-  </template>
-  
-  <script setup></script>
-  
-  <style scoped lang="sass"></style>
+  <div class="enterprise-development">
+    <div class="top">
+      <img src="../assets/imgs/banner_6.png" />
+    </div>
+    <h1>发展历程</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+.enterprise-development {
+  .top {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 300px;
+    overflow: hidden;
+    img {
+      width: 100%;
+    }
+  }
+
+  h1 {
+    text-align: center;
+    margin: 50px auto;
+  }
+}
+</style>
